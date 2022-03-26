05d91c3430588a6ef353f6e73439c413caa14387
Your Name <you@example.com>
2022-03-26 20:12:32 -0300

    Blocos e Sentenças

---
+++ desafio_1/desafio_sentença_bloco.js
@@ -0,0 +1,19 @@
+{//BLOCO DE CÓDIGO
+
+    //SENTENÇAS
+    console.log("NÚMEROS ÍMPARES:");
+    console.log("1")
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9 \n");
+}
+
+{
+    console.log("NÚMEROS PARES:");
+    console.log("2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+    console.log("10 \n");
+}
